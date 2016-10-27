8bb128d68f7387427532ba857e1cca9576e83939
Nick Blomberg <nick.blomberg@gmail.com>
2016-10-27 22:38:50 +0100

    Add temporary logout button to main activity

--- app/src/main/java/com/nickblomberg/kivation/activities/MainActivity.java
+++ app/src/main/java/com/nickblomberg/kivation/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.nickblomberg.kivation.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.nickblomberg.kivation.R;
@@ -19,6 +21,7 @@ public class MainActivity extends BaseActivity {
 
     private SessionManager sessionManager;
     @BindView(R.id.login_status) @Nullable TextView mLoginStatus;
+    @BindView(R.id.button_logout) @Nullable Button mLogoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,14 @@ public class MainActivity extends BaseActivity {
         if (credentials.length == 2) {
             mLoginStatus.setText("\nToken: " + token + "\n\nSecret: " + secret);
         }
+
+        // Temporary logout button for debugging
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sessionManager.logout();
+            }
+        });
     }
 
     @Override
